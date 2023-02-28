c9abfdc7236bae5ab3ec95b9dbc1989ba5b78b8a
Evaldo Cunha F <97982032+evaldocunhaf@users.noreply.github.com>
2023-02-27 21:57:47 -0300

    continuando nos desafios do curso em video

---
+++ curso em video 2/numero_por_extenso.py
@@ -0,0 +1,3 @@
+numeros = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez')
+escolha = int(input('digite o numero que voce quer por extenso: \n'))
+print(numeros[escolha])
---
+++ curso em video 2/tabela do brasileirao.py
@@ -0,0 +1,8 @@
+tabela = ('america_mg', 'athletico_pr', 'athletico_mg', 'bahia', 'botafogo', 'corinthians', 'coritiba', 'cruzeiro', 'cuiaba', 'mengao', 'fluminense', 'fortaleza', 'goias', 'gremio', 'internacional', 'palmeiras', 'bragantino', 'santos', 'sao paulo', 'vasco')
+for c in range(0, 5):
+    print(tabela[c])
+print('==========')
+
+for n in range(-1, -5, -1):
+    print(tabela[n])
+
